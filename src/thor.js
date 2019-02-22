3e89dfdd7c50afd8b278586991fa73251a522a96
brady <brady.m.adams@gmail.com>
2019-02-22 09:34:41 -0700

    prod and dev build

--- src/thor.js
+++ src/thor.js
@@ -4,10 +4,12 @@ const Material = require('./material');
 const Micro = require('./micro');
 const API = require('./api');
 
-module.exports = {
+const thor = {
   FEA: FEA,
   Material: Material,
   Micro: Micro,
   Hardware: Hardware,
   API: API
-}
+};
+
+module.exports = thor;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
+base_config = {
   entry: './src/thor.js',
-  mode: 'production',
   target: 'web',
   externals: {
     xmlhttprequest: 'XMLHttpRequest'
@@ -11,13 +10,32 @@ module.exports = {
   plugins: [
     new webpack.IgnorePlugin(/xmlhttprequest/),
   ],
-  output: {
-    filename: 'thor.js',
-    library: 'thor',
-    path: path.resolve(__dirname, 'build')
-  },
   node: {
     fs: 'empty',
     child_process: 'empty'
   }
 };
+
+prod_config = {
+  mode: 'production',
+  output: {
+    filename: 'thor.js',
+    library: 'thor',
+    path: path.resolve(__dirname, 'build')
+  }
+};
+
+Object.assign(prod_config, base_config);
+
+dev_config = {
+  mode: 'development',
+  output: {
+    filename: 'thor.dev.js',
+    library: 'thor',
+    path: path.resolve(__dirname, 'build')
+  }
+};
+
+Object.assign(dev_config, base_config);
+
+module.exports = [prod_config, dev_config];
