af729874e4ff035e77f9531bb82351237be650c4
colman-hartinger <colman.hartinger@tetonsim.com>
2021-04-07 13:51:00 -0600

    cleanup

--- src/thor.ts
+++ src/thor.ts
@@ -1,11 +1,9 @@
 export {};
 
-const API = require('./api');
-// import { Job } from './job';
+const API = require('./api')
 
 const thor = {
   API: API,
-  // Job: Job,
 };
 
 module.exports = thor;
