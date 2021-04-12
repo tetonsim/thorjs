c9cf62992c579d92ba8e3bfc5188887386a2f6be
colman-hartinger <colman.hartinger@tetonsim.com>
2021-04-12 15:19:44 -0600

    cleanup

--- src/chop/model/load.ts
+++ src/chop/model/load.ts
@@ -1,5 +1,3 @@
-export type Int = number & { __int__: void };
-
 /**
  * Defines a load upon a given portion of the model geometry.
  * The type attribute represents the type of load. The only, current, boundary condition type is "force".
@@ -20,17 +18,17 @@ export interface Load {
   /**
    * A list of face ids on "mesh" that this load is applied to.
    */
-  face: Array<Int>;
+  face: Array<number>;
   /**
    * The applied force vector in Newtons.
    */
-  force: [number, number, number];
+  force: Array<[number, number, number]>;
 }
 
 export class Load {
   type = 'force';
   name = 'load1';
   mesh = 'mesh';
-  face = [0 as Int];
-  force: [number, number, number] = [1, 0, 0];
+  face = [0];
+  force: [number, number, number][] = [[1, 0, 0]];
 }
